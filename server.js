68a311f0fd0ce68df4b37ad72d8e2fbf4f2fab78
esskayaitch <simonhillskh@gmail.com>
2021-11-20 19:46:17 +1100

    Prepared for deploy.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express'); // common js format
 const connectDB = require('./config/db');
-
-const connctDB = require('./config/db');
+const Path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5555;
@@ -20,5 +19,13 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper' }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in Production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(Path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`- Express server running on port ${PORT} - `));
